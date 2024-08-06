6500f14daf114386d4b3cdb9e32f1c9180daffe5
Temirlan Termechikov <temirlan.termechikov@gmail.com>
2024-08-06 11:49:23 +0600

    Changed some settings

--- src/data/services/home-page-service.ts
+++ src/data/services/home-page-service.ts
@@ -6,7 +6,7 @@ export async function getHomePageData() {
   const url = new URL('/api/home-page?populate=deep', baseUrl);
 
   try {
-    const response = await fetch(url.href);
+    const response = await fetch(url.href, { cache: 'no-cache' });
     const jsonData = await response.json();
     const data = jsonData.data.attributes;
 
